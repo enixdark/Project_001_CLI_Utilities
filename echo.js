365b51d1b541d5956c8ae8bbd244d80d8592dd2f
enixdark <cqshinn92@gmail.com>
2016-10-16 01:39:09 +0700

    finish echo cli

--- echo.js
+++ echo.js
@@ -6,10 +6,11 @@ let fs = require('fs').promise
 async function echo() {
     // Use 'await' in here
     // Your implementation here
-    console.log(await fs.readFile(__filename, console.log))
-
-    let promise = fs.readdir(__dirname)
-    let fileNames = await promise
+    // console.log(await fs.readFile(__filename, console.log))
+    let argv = await process.argv
+    if(argv.length > 2){
+      process.stdout.write(argv.slice(2).join(' ') + "\n")
+    }
 }
 
 echo()
